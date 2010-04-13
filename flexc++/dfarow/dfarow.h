9bbdba244c3b71f177d5e2ba603fe3a6bf097649
Frank B. Brokken <f.b.brokken@rug.nl>
2010-04-13 13:31:04 +0200

    Added data members to DFARow

--- flexc++/dfarow/dfarow.h
+++ flexc++/dfarow/dfarow.h
@@ -1,9 +1,13 @@
 #ifndef INCLUDED_DFAROW_
 #define INCLUDED_DFAROW_
 
+#include <unordered_map>
 
 class DFARow
 {
+    size_t d_final;     // matched rule when this state cannot continue
+    std::unordered_map<size_t, size_t> d_transit;
+    
     public:
         DFARow();
 
