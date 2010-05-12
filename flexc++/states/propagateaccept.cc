5295aad0c56c9468ec7036724f425e2a8421ad91
Frank B. Brokken <f.b.brokken@rug.nl>
2010-05-12 16:48:18 +0200

    Implementing the LA procedure of the README.lookahead file

---
+++ flexc++/states/propagateaccept.cc
@@ -0,0 +1,31 @@
+#include "states.ih"
+
+#include <iostream>     // TEMPO
+
+void States::propagateAccept(int state, int count)
+{
+    if (state == 0 || contains(state))
+        return;
+    
+    d_ruleStates.push_back(state);
+
+    cerr << "States: propagate " << state << ", " << count << endl;
+
+
+    State &curState = d_state[state];
+                                        // always store the max. accept 
+                                        // count: received or current value
+    curState.setAccept(count = max(curState.accept(), count));
+
+    count += curState.type() != State::EMPTY;
+
+    int next = curState.next1();
+    if (!contains(next));
+        propagateAccept(next, count);
+    next = curState.next2();
+    if (!contains(next))
+        propagateAccept(next, count);
+
+    d_ruleStates.pop_back();
+}
+
