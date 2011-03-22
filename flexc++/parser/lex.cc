6baebf5b5e39e44dcc3a26867c63138f0c228169
Frank B. Brokken <f.b.brokken@rug.nl>
2011-03-22 10:43:25 +0100

    BLOCK tokens are now returned *after* eating trailing WS

--- flexc++/parser/lex.cc
+++ flexc++/parser/lex.cc
@@ -1,9 +1,54 @@
 #include "parser.ih"
 
+namespace{
+    char const *symbol[] = 
+    {
+        "EXCL_START_CONDITION",
+        "INCL_START_CONDITION",
+        "NAME",
+        "SECTION_DELIMITER",
+        "BASECLASSHEADER",
+        "CLASSHEADER",
+        "CLASSNAME",
+        "DEBUG",
+        "DEBUG_STEPS",
+        "IMPLEMENTATIONHEADER",
+        "INTERACTIVE",
+        "LEXFUNCTIONNAME",
+        "LEXSOURCE",
+        "NAMESPACE",
+        "OPTION_DIRECTIVE",
+        "SKELETON_DIRECTORY",
+        "BLOCK",
+        "WS",
+        "PREDEFINED_CLASS",
+        "CC_PLUS",
+        "CC_MINUS",
+        "CHARACTER",
+        "DECIMAL",
+        "CC_NEGATED",
+        "CC_START",
+        "ESCAPE_SEQUENCE",
+        "STRING",
+        "EOF_PATTERN",
+    };
+}
+
 int Parser::lex()
 {
     print();
     ++d_patternTokenCount;
-    return d_scanner.pLex();
+//    return d_scanner.pLex();
+
+    int token = d_scanner.pLex();
+    cout << "Token: " << token << ": ";
+    if (isprint(token))
+        cout << "char `" << static_cast<char>(token) << '\'';
+    else if (token >= 0x100)
+        cout << "symbol `" << symbol[token - 257] << '\'';
+
+    cout << '\n';
+
+    return token;
 }
 
