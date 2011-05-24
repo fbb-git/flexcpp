99444eb530768bc8bc03e1ff9a70c8e9d2cd73d0
Frank B. Brokken <f.b.brokken@rug.nl>
2011-05-24 16:22:43 +0200

    Corrected rule linenr off-by-one

--- flexc++/rules/add.cc
+++ flexc++/rules/add.cc
@@ -1,11 +1,14 @@
 #include "rules.ih"
 
+    // lineNr is always 1 too large as the parser detects the end of the rule
+    // at the line following the rule's definition. Therefore lineNr is
+    // here reduced by 1.
 void Rules::add(bool bol, spSemVal const &patternVal, Block const &block,
                 string const &fileName, size_t lineNr)
 {
     Pair const &pair = SemVal::downCast<PatternVal>(*patternVal).pair();
 
-    d_rules.push_back( { d_states, bol, pair, block, fileName, lineNr } );
+    d_rules.push_back( { d_states, bol, pair, block, fileName, lineNr - 1 } );
 
     size_t ruleIdx = d_rules.size() - 1;
 
