ab6635989003d71f5074df5511645b1affff514f
Frank B. Brokken <f.b.brokken@rug.nl>
2011-08-05 06:17:40 +0200

    added overlooked state/dup.cc file required for ESCAPE_SEQUENCE handling

---
+++ flexc++/state/dup.cc
@@ -0,0 +1,9 @@
+#include "state.ih"
+
+void State::dup(State const &src, size_t next1, size_t next2)
+{
+    d_data.reset(src.data().clone(next1, next2));
+    d_type = src.d_type;
+    d_flag = src.d_flag;
+    d_rule = src.d_rule;
+}
