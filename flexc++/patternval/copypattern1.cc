45e004bbed2179087b5a2d85cbc264994c8939a0
Frank B. Brokken <f.b.brokken@rug.nl>
2012-01-25 15:18:35 +0100

    decorated _interval and _quantifier use

---
+++ flexc++/patternval/copypattern1.cc
@@ -0,0 +1,23 @@
+#include "patternval.ih"
+
+void PatternVal::copyPattern(States &states, size_t count, 
+                                                PairVector &beginEnd)
+{
+    size_t begin = beginEnd[0].first;
+    size_t end = beginEnd[0].second;
+
+    for ( ; count--; )       // copy 'count' patterns
+    {
+        Map copied;     // a map is used to related old state indices (index)
+                        // to new state indices (values). The value 0, 0
+                        // indicates the end of  link and is always initially
+                        // added. 
+        copied[0] = 0;
+
+                                                    // duplicate the pattern
+        dupPattern(copied, states, begin);
+
+        beginEnd.push_back(States::Pair{copied[begin], copied[end]});
+    }
+}
+
