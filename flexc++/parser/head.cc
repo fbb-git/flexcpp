a30c52be5e46e7fc81e72051a111557f6656c35f
Frank B. Brokken <f.b.brokken@rug.nl>
2012-01-27 16:47:58 +0100

    Removed IDENTIFIER from the INITIAL scanner specifications     IDENTIFIER is only required in the preamble (pre), and after that identifier characters should be processed individually, to allow for constructions like ab+. This is still required for DECIMAL matches.     IDENTIFIER also removed from parser/inc/rules and parser/inc/characterclass.     Now the IDENTIFIER's individual characters are returned as CHAR or as combi_chars values, which is OK.

--- flexc++/parser/head.cc
+++ flexc++/parser/head.cc
@@ -2,9 +2,8 @@
 
 spSemUnion Parser::head()
 {
-    d_scanner.accept(1);                // accept the 1st char
-
-    return rawText();
+    d_scanner.accept(1);                // accept the 1st char, redo the rest
+    return rawText();                   // see inc/regex for details
 }
 
 
