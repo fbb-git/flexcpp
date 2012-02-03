30698729ee973a04a9da02a8e22ad7b87e8c50e4
Frank B. Brokken <f.b.brokken@rug.nl>
2012-02-03 11:19:40 +0100

    WIP: [ // xxx] and reflex OK

--- flexc++/scanner/handleeolncomment.cc
+++ flexc++/scanner/handleeolncomment.cc
@@ -2,9 +2,9 @@
 
 bool Scanner::handleEOLNcomment()
 {
-//    if (not d_inCharClass)
+    if (not d_inCharClass)
         return false;
 
-    accept(matched().find("//") + 1);   // in char class: accept blanks + //
+    accept(matched().find("//") + 2);   // in char class: accept blanks + //
     return true;                        // and rescan the rest
 }
