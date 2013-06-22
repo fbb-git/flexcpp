dc395bd5c0ba1e9b8d03a99cbfd457ce4446aaa4
Frank B. Brokken <f.b.brokken@rug.nl>
2013-06-22 17:17:25 +0200

    setIncTailCount no longer sets/inspects TailCount::PROCESSED.   Reflex OK Since setTailCount pre-inspects PROCESSED, this flas is no longer handled This function now only has one statement (remove?)

--- flexc++/dfa/setinctailcount.cc
+++ flexc++/dfa/setinctailcount.cc
@@ -2,13 +2,14 @@
 
 bool DFA::setIncTailCount(TailCount::Type type, TailCount &thisTailCount)
 {
-    if (type & TailCount::PROCESSED)         // already handled this entry
-        return false;
+//    if (type & TailCount::PROCESSED)         // already handled this entry
+//        return false;
 
 //cout << "setIncTailCount: incrementing Acc Count for rule " << 
 //thisTailCount.rule() << '\n';
 
-    thisTailCount.addFlag(TailCount::PROCESSED | TailCount::INCREMENTING);
+//    thisTailCount.addFlag(TailCount::PROCESSED | TailCount::INCREMENTING);
+    thisTailCount.addFlag(TailCount::INCREMENTING);
 
     return true;
 }
