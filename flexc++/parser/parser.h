cb85556c43d49f2a9fe832dbc182861971269daa
Frank B. Brokken <f.b.brokken@rug.nl>
2014-01-08 20:10:17 +0100

    removed SF parser::d_userStartConditions

--- flexc++/parser/parser.h
+++ flexc++/parser/parser.h
@@ -48,9 +48,6 @@ class Parser: public ParserBase
     Rules &d_rules;
     States &d_states;
 
-    size_t d_nUserStartConditions = 0;      // the number of defined used
-                                            // start conditions (including
-                                            //INITIAL) 
     size_t d_lopStartCondition = 0;         // startconditions index for 
                                             // the next LOP
 
