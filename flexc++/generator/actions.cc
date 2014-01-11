1487255a32453dc522850b0b2be2140126f59f14
Frank B. Brokken <f.b.brokken@rug.nl>
2014-01-11 13:53:31 +0100

    Actions correctly related to rules, next optimize executeAction__

--- flexc++/generator/actions.cc
+++ flexc++/generator/actions.cc
@@ -4,6 +4,7 @@ void Generator::actions(ostream &out) const
 {
     key(out);
 
+
     // First handle the non-LOP rule actions:
     // this allows falling throug if rules are using OR actions
 
@@ -12,54 +13,58 @@ void Generator::actions(ostream &out) const
                 RuleType::LOP_1
             };
 
-// mutable!!
+// TEMPORARILY mutable!!
 d_lineDirectives = false;
 
-    for (RuleType type: blockTypes)
-    {
         size_t idx = 0;
         for (auto &rule: ranger(d_rules.ruleBegin(), d_rules.ruleEnd()))
         {
-            if (rule.type() == type)
                 ruleAction(rule.block(), out, idx);
             ++idx;
         }
-    }    
-
-    static RuleType fallThroughTypes[] = {
-                RuleType::LOP_2,
-                RuleType::LOP_3,
-                RuleType::LOP_4,
-            };
-
-    for (RuleType type: fallThroughTypes)
-    {
-        auto end = d_rules.ruleEnd();
-        auto rule = find_if(d_rules.ruleBegin(), end, 
-            [&](Rule const &rule)
-            {
-                return rule.type() == type;
-            }
-        );
-
-        if (rule == end)
-            continue;
-
-        size_t caseLabel = end - rule;  // eventual case label
-        size_t idx = caseLabel + 1;     // iterates in parallel with 'next':
-
-        for (auto next = rule + 1; next != end; ++next, ++idx)
-        {
-            if (next->type() == type)
-                out << "        case " << idx << ":\n";
-        }
-
-        ruleAction(rule->block(), out, caseLabel);
-    }    
+//
+//    for (RuleType type: blockTypes)
+//    {
+//        size_t idx = 0;
+//        for (auto &rule: ranger(d_rules.ruleBegin(), d_rules.ruleEnd()))
+//        {
+//            if (rule.type() == type)
+//                ruleAction(rule.block(), out, idx);
+//            ++idx;
+//        }
+//    }    
+//
+//    static RuleType fallThroughTypes[] = {
+//                RuleType::LOP_2,
+//                RuleType::LOP_3,
+//                RuleType::LOP_4,
+//            };
+//
+//    for (RuleType type: fallThroughTypes)
+//    {
+//        auto end = d_rules.ruleEnd();
+//        auto rule = find_if(d_rules.ruleBegin(), end, 
+//            [&](Rule const &rule)
+//            {
+//                return rule.type() == type;
+//            }
+//        );
+//
+//        if (rule == end)
+//            continue;
+//
+//        size_t caseLabel = end - rule;  // eventual case label
+//        size_t idx = caseLabel + 1;     // iterates in parallel with 'next':
+//
+//        for (auto next = rule + 1; next != end; ++next, ++idx)
+//        {
+//            if (next->type() == type)
+//                out << "        case " << idx << ":\n";
+//        }
+//
+//        ruleAction(rule->block(), out, caseLabel);
+//    }    
 }
-
-
-
 
 
 
