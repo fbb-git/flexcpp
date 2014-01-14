50a769e282040e9fdf1b4964b9a9b595e1a6a68a
Frank B. Brokken <f.b.brokken@rug.nl>
2014-01-14 09:49:50 +0100

    Changed sequence of "...\n" strings into R"( ... )" string

--- flexc++/generator/inputinterface.cc
+++ flexc++/generator/inputinterface.cc
@@ -10,33 +10,35 @@ void Generator::inputInterface(std::ostream &out) const
         out << "#include \"" << interface << "\"\n";
     else
         out <<
-"    class Input\n"
-"    {\n"
-"        std::deque<unsigned char> d_deque;  // pending input chars\n"
-"        std::istream *d_in;                 // ptr for easy streamswitching\n"
-"        size_t d_lineNr;                    // line count\n"
-"\n"
-"        public:\n"
-"            Input();\n"
-"                                       // iStream: dynamically allocated\n"
-"            Input(std::istream *iStream, size_t lineNr = 1);\n"
-"            size_t get();                   // the next range\n"
-"            void reRead(size_t ch);         // push back 'ch' (if < 0x100)\n"
-"                                            // push back str from idx "
-                                                                "'fmIdx'\n"
-"            void reRead(std::string const &str, size_t fmIdx);\n"
-"            size_t lineNr() const\n"
-"            {\n"
-"                return d_lineNr;\n"
-"            }\n"
-"            void close()                    // force closing the stream\n"
-"            {\n"
-"                delete d_in;\n"
-"                d_in = 0;                   // switchStreams also closes\n"
-"            }\n"
-"\n"
-"        private:\n"
-"            size_t next();                  // obtain the next character\n"
-"    };\n";
+R"(
+    class Input
+    {
+        std::deque<unsigned char> d_deque;  // pending input chars
+        std::istream *d_in;                 // ptr for easy streamswitching
+        size_t d_lineNr;                    // line count
+
+        public:
+            Input();
+                                       // iStream: dynamically allocated
+            Input(std::istream *iStream, size_t lineNr = 1);
+            size_t get();                   // the next range
+            void reRead(size_t ch);         // push back 'ch' (if < 0x100)
+                                            // push back str from idx 'fmIdx'
+            void reRead(std::string const &str, size_t fmIdx);
+            size_t lineNr() const
+            {
+                return d_lineNr;
+            }
+            void close()                    // force closing the stream
+            {
+                delete d_in;
+                d_in = 0;                   // switchStreams also closes
+            }
+
+        private:
+            size_t next();                  // obtain the next character
+    };
+)";     
+// >>>> R"( section ends <<<<
 
 }
