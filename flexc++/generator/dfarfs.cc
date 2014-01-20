adaeccf293e7b8f02b8b06e97307d276a8292fcd
Frank B. Brokken <f.b.brokken@rug.nl>
2014-01-20 21:45:05 +0100

    WIP on the manual

--- flexc++/generator/dfarfs.cc
+++ flexc++/generator/dfarfs.cc
@@ -25,18 +25,6 @@ void Generator::dfaRFs(DFARow const &row, ostream &out,
     }
 
     out << setw(3) << rule << ", " << flag;
-    
-//    out << setw(2) << rf.size() << ',';        // begin index in s_rfc__
-//
-//    auto final = row.final();
-//
-//    if (final.first != numeric_limits<size_t>::max())
-//        rf.push_back(RuleFlag {final.first, FINAL | BOL});
-//
-//    if (final.second != numeric_limits<size_t>::max())
-//        rf.push_back(RuleFlag {final.second, FINAL});
-//
-//    out << setw(2) << rf.size();               // end index in s_rf__
 }
 
 
