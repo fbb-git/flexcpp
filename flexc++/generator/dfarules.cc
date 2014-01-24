4f79fb4abb233364a113f331cd2143b33c797b8f
Frank B. Brokken <f.b.brokken@rug.nl>
2014-01-24 11:20:54 +0100

    Cosmetics

--- flexc++/generator/dfarules.cc
+++ flexc++/generator/dfarules.cc
@@ -8,7 +8,8 @@ void Generator::dfaRules(DFARow const &row, ostream &out)
         setw(3) << 
             (final.first != max<size_t>() ? 
                 static_cast<int>(final.first) : -1) << ',' << 
-        setw(3) << (final.second != max<size_t>() ? 
+        setw(3) << 
+            (final.second != max<size_t>() ? 
                 static_cast<int>(final.second) : -1);
 }
 
