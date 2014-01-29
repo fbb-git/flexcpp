d9e8f7e9ddd3dba617d4bd12546f362719dea032
Frank B. Brokken <f.b.brokken@rug.nl>
2014-01-29 21:50:52 +0100

    Reflex OK. Removed SF test for already generated DFA from generator/dfa.cc

--- flexc++/generator/dfa.cc
+++ flexc++/generator/dfa.cc
@@ -6,14 +6,6 @@ void Generator::dfa(DFAs::Pair const &dfaPair, ostream &out,
                     vector<string> &startStates,
                     vector<size_t> &dfaOffsets)
 {
-    if 
-    (
-        find(startStates.begin(), startStates.end(), dfaPair.first) 
-        !=
-        startStates.end()
-    )
-        return;
-
     out << "    // " << dfaPair.first << '\n';  // name of the DFA is shown
     startStates.push_back(dfaPair.first);
     dfaOffsets.push_back(*(dfaOffsets.rbegin()) + dfaPair.second->size());
