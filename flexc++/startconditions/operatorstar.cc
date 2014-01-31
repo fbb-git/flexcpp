8b1a2a15478fa23ded231a6e08216916b9d94155
Frank B. Brokken <f.b.brokken@rug.nl>
2014-01-31 12:08:01 +0100

    StartConditions::operator* removed (SF)

--- flexc++/startconditions/operatorstar.cc
+++
@@ -1,8 +0,0 @@
-#include "startconditions.ih"
-
-StartConditions::NameVector 
-                    const &StartConditions::const_iterator::operator*() const
-{
-    d_nameVector = {(*d_iter).first, (*d_iter).second.d_rules};
-    return d_nameVector;
-}
