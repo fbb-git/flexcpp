c3bb38b00ec055c83d7d5b4369b524931a10034f
Frank B. Brokken <f.b.brokken@rug.nl>
2014-02-05 16:44:36 +0100

    implementing \- recognition in CharClass escape-strings

--- flexc++/charclass/escape.cc
+++ flexc++/charclass/escape.cc
@@ -2,6 +2,32 @@
 
 CharClass CharClass::escape(string const &match)
 {
-    CharClass ret(String::unescape(match));
+    vector<string> substr;
+
+    size_t begin = 0;
+    size_t idx = 0;                       // push \- delimited substrings
+    while ((idx = match.find("\\", idx)) != string::npos)
+    {
+        if (match[idx + 1] != '-')
+        {
+            idx += 2;
+            continue;
+        }
+
+        substr.push_back(
+                    String::unescape(match.substr(begin, idx - begin)));
+
+        idx += 2;
+        begin = idx;
+    }
+
+    substr.push_back(String::unescape(match.substr(begin, string::npos)));
+
+    string str = substr[0];
+
+    for (idx = 1; idx != substr.size(); ++idx)
+        str += "\\-" + substr[idx];
+
+    CharClass ret(str);
     return ret;
 }
