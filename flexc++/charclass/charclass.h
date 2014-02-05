0be1e2336dbd85b5f0d9297c4a7336869991453d
Frank B. Brokken <f.b.brokken@rug.nl>
2014-02-05 10:57:30 +0100

    Added comment to charclass/charclass.h

--- flexc++/charclass/charclass.h
+++ flexc++/charclass/charclass.h
@@ -10,27 +10,57 @@ class CharClass
 {
     friend std::ostream &operator<<(std::ostream &out, CharClass const &cc);
 
+        // the bool indicates that the char belongs to a predefined range.
+        // this is used when merging elements of the set: a '-' cannot follow 
+        // or be followed by characters from a predefined range.
     typedef std::vector<std::pair<char, bool>> Vector;
     Vector d_chars;
 
     public:
         CharClass() = default;
-        std::string str() const;        // the chars in the set
+        std::string str() const;        // Determine the (final) set of
+                                        // characters (using `set()') and 
+                                        // return them as a std::string
 
+                                        // create a class containing the 
+                                        // the set of chars defined by 'chars'
+                                        // startign at idx 'skip'
+        static CharClass elements(std::string const &chars, size_t skip);
+
+                                        // add the set of chars in rhs to
+                                        // the current set of chars
+        CharClass &concatenate(CharClass const &rhs);
+
+                                        // Call unescape on match, and return
+                                        // the CharClass containing the 
+                                        // resulting characters
+        static CharClass escape(std::string const &match);
+
+                                        // use CharRange to determine the set
+                                        // of predefined chars, and return the
+                                        // set as a CharClass object.
+        static CharClass predefined(std::string const &range);
+                
+        CharClass negate() const;       // pass the current set of characters
+                                        // (determined by set()) to
+                                        // CharRange::negate, and return the
+                                        // resulting CharClass
+
+                                        // Concatenate the current set and rhs
+                                        // and return the CharClass returned
+                                        // by negate().
+        CharClass negate(CharClass const &rhs);
+
+                                        // merge two existing sets
+                                        // return a new one
         static CharClass unite(CharClass const &lhs, CharClass const &rhs);
+
+                                        // return a new set containing the lhs
+                                        // elements not found in the rhs set
         static CharClass difference(CharClass const &lhs, 
                                     CharClass const &rhs);
 
-        static CharClass elements(std::string const &chars, size_t skip);
-        CharClass &concatenate(CharClass const &rhs);
-
-        static CharClass escape(std::string const &match);
-        static CharClass predefined(std::string const &range);
-                
-        CharClass negate() const;
-        CharClass negate(CharClass const &rhs);
-
-        bool empty() const;
+        bool empty() const;             // true if the set is empty.
 
     private:
         CharClass(char ch);
