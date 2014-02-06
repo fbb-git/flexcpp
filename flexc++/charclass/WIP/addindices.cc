8e8d7cf4198baaec9620d2bf291f0b3a38ad6a0f
Frank B. Brokken <f.b.brokken@rug.nl>
2014-02-06 20:02:16 +0100

    Reflex OK: CharClass reimplemented

--- flexc++/charclass/WIP/addindices.cc
+++
@@ -1,9 +0,0 @@
-#include "charclass.ih"
-
-void CharClass::addChars(std::set<char> &dest, size_t begin, size_t end) const
-{
-//    cout << "\tADD INDICES: " << begin << " up to " << end << '\n';
-
-    for (; begin != end; ++begin)
-        dest.insert(d_chars[begin].first);
-}
