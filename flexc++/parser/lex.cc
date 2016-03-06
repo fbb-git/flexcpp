8e5321cb872c0f5cad199508a6bdd6b3fed7ca80
Frank B. Brokken <f.b.brokken@rug.nl>
2016-03-06 09:37:35 +0100

    Reflex OK, Regression tests OK.     Cleanup of parser/lex.cc's code: Parser::lex() now merely returns     d_scanner's tokens until EOF (token 0). Then it returns \n, setting     d_nlAtEOF to true. At the next call of Parser::lex() 0 is returned     because of d_nlAtEOF == true, ending the parsing process.

--- flexc++/parser/lex.cc
+++ flexc++/parser/lex.cc
@@ -5,25 +5,6 @@ int Parser::lex()
     print();
     ++d_tokenCount;
 
-//    if (d_nlAtEOF)
-//        return 0;
-//
-//    if (d_nlAfterBlock)
-//    {
-//        d_nlAfterBlock = false;
-//        return '\n';
-//    }
-//
-//    int token = d_scanner.pLex();
-//
-//    if (token == BLOCK) 
-//       d_nlAfterBlock = true;
-//    else if (token == 0)
-//    {
-//        d_nlAtEOF = true;
-//        return '\n';
-//    }
-
     if (d_nlAtEOF)
         return 0;
 
