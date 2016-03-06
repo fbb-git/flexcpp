fccca37489cb74516aab95a9d935a763cf95882c
Frank B. Brokken <f.b.brokken@rug.nl>
2016-03-06 09:33:35 +0100

    Simplified the missing final EOLN implementation. Reflex, regression OK

--- flexc++/parser/lex.cc
+++ flexc++/parser/lex.cc
@@ -2,28 +2,34 @@
 
 int Parser::lex()
 {
-//    static bool nlAfterBlock = false;        // to compensate for  missing \n at
-//                                        // the end of crippled input files
-//
-//    static bool nlAtEOF = false;
-
     print();
     ++d_tokenCount;
+
+//    if (d_nlAtEOF)
+//        return 0;
+//
+//    if (d_nlAfterBlock)
+//    {
+//        d_nlAfterBlock = false;
+//        return '\n';
+//    }
+//
+//    int token = d_scanner.pLex();
+//
+//    if (token == BLOCK) 
+//       d_nlAfterBlock = true;
+//    else if (token == 0)
+//    {
+//        d_nlAtEOF = true;
+//        return '\n';
+//    }
 
     if (d_nlAtEOF)
         return 0;
 
-    if (d_nlAfterBlock)
-    {
-        d_nlAfterBlock = false;
-        return '\n';
-    }
-
     int token = d_scanner.pLex();
 
-    if (token == BLOCK) 
-       d_nlAfterBlock = true;
-    else if (token == 0)
+    if (token == 0)
     {
         d_nlAtEOF = true;
         return '\n';
