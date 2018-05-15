4ef6e126bd649b1cfa657b895484454308950304
Frank B. Brokken <f.b.brokken@rug.nl>
2018-05-15 12:35:04 +0200

    setNameSpaceIdentifier must unconditionally be called

--- flexc++/options/setaccessorvariables.cc
+++ flexc++/options/setaccessorvariables.cc
@@ -32,10 +32,9 @@ void Options::setAccessorVariables()
                     MAX_DEPTH;
 
     if (arg.option(&d_nameSpace, 'n'))
-    {
         s_warnOptions.insert("namespace");
-        setNameSpaceIdentifier();
-    }
+
+    setNameSpaceIdentifier();
 
     // Skeletons
     if (!arg.option(&d_skeletonDirectory, 'S') && d_skeletonDirectory.empty())
